ca6ae3d3384924afaad8510c9c6f837d9ae0466d
muniecodechristian <muniecodechristian@gmail.com>
2026-09-10 15:16:06 +0100

    Change language attribute from 'en' to 'fr'

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,12 +16,15 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Tech+ RDC | Communication et stratégie",
   description: "Tech+ RDC accompagne les marques et organisations congolaises.",
+  icons: {
+    icon: "/tech-plus-logo.jpg",
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
+      lang="fr"
       className={`${manrope.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col">
